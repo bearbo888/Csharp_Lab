6445e2effbdae46be802553c1cf2e5b2c520287d
Hank Hsu <bearbo888@yahoo.com.tw>
2022-03-23 00:33:47 +0800

    XO GAME FINISH

---
+++ XOgame/frmXOG.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace XOgame
+{
+    partial class frmXOG
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btnRet = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(55, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "XO GAME";
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(139, 106);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(70, 70);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn3.Location = new System.Drawing.Point(215, 106);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(70, 70);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(63, 106);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(70, 70);
+            this.btn1.TabIndex = 3;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn4.Location = new System.Drawing.Point(63, 182);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(70, 70);
+            this.btn4.TabIndex = 6;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn6.Location = new System.Drawing.Point(215, 182);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(70, 70);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn5.Location = new System.Drawing.Point(139, 182);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(70, 70);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn7.Location = new System.Drawing.Point(63, 258);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(70, 70);
+            this.btn7.TabIndex = 9;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn9.Location = new System.Drawing.Point(215, 258);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(70, 70);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn8.Location = new System.Drawing.Point(139, 258);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(70, 70);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnRet
+            // 
+            this.btnRet.Location = new System.Drawing.Point(63, 353);
+            this.btnRet.Name = "btnRet";
+            this.btnRet.Size = new System.Drawing.Size(107, 46);
+            this.btnRet.TabIndex = 10;
+            this.btnRet.Text = "&Reset(R)";
+            this.btnRet.UseVisualStyleBackColor = true;
+            this.btnRet.Click += new System.EventHandler(this.btnRet_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(176, 353);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(107, 46);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "&Exit(e)";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmXOG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRet);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmXOG";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmXOG_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btnRet;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ XOgame/frmXOG.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOgame
+{
+    public partial class frmXOG : Form
+    {
+        public frmXOG()
+        {
+            InitializeComponent();
+
+        }
+
+        bool val = false; int count = 0;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Text = (val) ? "X" : "O";
+            val = !val;
+            count++;
+            b.Enabled = false;
+            if (count > 5) { Winner(); }
+        }
+
+        private void Winner()
+        {
+            bool win = false;
+
+            if (btn1.Text == btn2.Text && btn2.Text == btn3.Text && !String.IsNullOrWhiteSpace(btn1.Text)) win = !win;
+            if (btn4.Text == btn5.Text && btn5.Text == btn6.Text && !String.IsNullOrWhiteSpace(btn4.Text)) win = !win;
+            if (btn7.Text == btn8.Text && btn8.Text == btn9.Text && !String.IsNullOrWhiteSpace(btn7.Text)) win = !win;
+
+            if (btn1.Text == btn4.Text && btn4.Text == btn7.Text && !String.IsNullOrWhiteSpace(btn1.Text)) win = !win;
+            if (btn2.Text == btn5.Text && btn5.Text == btn8.Text && !String.IsNullOrWhiteSpace(btn2.Text)) win = !win;
+            if (btn3.Text == btn6.Text && btn6.Text == btn9.Text && !String.IsNullOrWhiteSpace(btn3.Text)) win = !win;
+
+            if (btn1.Text == btn5.Text && btn5.Text == btn9.Text && !String.IsNullOrWhiteSpace(btn1.Text)) win = !win;
+            if (btn3.Text == btn5.Text && btn5.Text == btn7.Text && !String.IsNullOrWhiteSpace(btn3.Text)) win = !win;
+
+            if (win) { MessageBox.Show("Win" + ((val) ? "O" : "X")); Reset(); }
+            if (count == 9 && win==false) { MessageBox.Show("平局"); }
+        }
+
+        private void Reset()
+        {
+            count = 0;
+            val = false;
+            btn1.Text = btn2.Text = btn3.Text = btn4.Text = btn5.Text = btn6.Text = btn7.Text = btn8.Text = btn9.Text = "";
+            btn1.Enabled = btn2.Enabled = btn3.Enabled = btn4.Enabled = btn5.Enabled = btn6.Enabled = btn7.Enabled = btn8.Enabled = btn9.Enabled = true;
+        }
+
+        private void btnRet_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void frmXOG_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case (Keys)27:
+                    Close();
+                    break;
+                case (Keys)82:
+                    Reset();
+                    break;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Environment.Exit(Environment.ExitCode);
+        }
+    }
+}
