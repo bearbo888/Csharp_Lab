4f0767c0d0bc9c3ac0dd7a38cf10382022e7486c
Hank Hsu <bearbo888@yahoo.com.tw>
2022-03-24 19:50:20 +0800

    guess

---
+++ GUESS/frmGUS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUESS
+{
+    public partial class frmGUS : Form
+    {
+        int ans = 0; bool win = false;
+
+        public frmGUS()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            ans = random.Next(1, 100);
+        }
+        public frmGUS(int guess)
+        {
+            if (guess > ans)
+            {
+                label2.Text = guess + "~100";
+            }else if (guess < ans)
+            {
+                label2.Text = "1~" + guess;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmPUT input = new frmPUT(ans,win);
+            input.Show();
+        }
+    }
+}
---
+++ GUESS/frmPUT.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUESS
+{
+    public partial class frmPUT : Form
+    {
+        int x = 0;
+        public frmPUT(int ans, bool win)
+        {
+            InitializeComponent();
+            x = ans;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmGUS guess = new frmGUS(int.Parse(textBox1.Text));
+            
+        }
+    }
+}
